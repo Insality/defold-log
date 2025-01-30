d78d7f80c8a9f61398a72651df4b4f8c14066c12
Insality <insality@gmail.com>
2025-01-30 21:21:36 +0200

    WIP Update default logger and easier library setup

--- log/log.lua
+++ log/log.lua
@@ -2,7 +2,7 @@
 local string_m = utf8 or string
 
 ---@class log
-local Log = {}
+local M = {}
 
 local TYPE_TABLE = "table"
 local IS_DEBUG = sys.get_engine_info().is_debug
@@ -12,43 +12,40 @@ local IS_MOBILE = SYSTEM_NAME == "iPhone OS" or SYSTEM_NAME == "Android"
 local DEFAULT_LEVEL = IS_DEBUG and "DEBUG" or "WARN"
 local GAME_LOG_LEVEL = sys.get_config_string(IS_DEBUG and "log.level" or "log.level_release", DEFAULT_LEVEL)
 
-local IS_TIME_TRACK = IS_DEBUG and sys.get_config_int("log.time_tracking", 0) == 1
-local IS_MEMORY_TRACK = IS_DEBUG and sys.get_config_int("log.memory_tracking", 0) == 1
+--local IS_TIME_TRACK = IS_DEBUG and sys.get_config_int("log.time_tracking", 0) == 1
+--local IS_MEMORY_TRACK = IS_DEBUG and sys.get_config_int("log.memory_tracking", 0) == 1
 local INFO_BLOCK_LENGTH = sys.get_config_int("log.info_block_length", 18)
+local LOGGER_BLOCK_WIDTH = sys.get_config_int("log.logger_block_width", 10)
 local MAX_LOG_LENGTH = sys.get_config_int("log.max_log_length", 1024)
 local INSPECT_DEPTH = sys.get_config_int("log.inspect_depth", 1)
 
-local LOGGER_PREFIX = ""
+--%time_tracking %memory_tracking %chronos_tracking
+-- Need to check if info block contains these flags
+local IS_TIME_TRACK = IS_DEBUG and string_m.find(sys.get_config_string("log.info_block", ""), "%%time_tracking") ~= nil
+local IS_MEMORY_TRACK = IS_DEBUG and string_m.find(sys.get_config_string("log.info_block", ""), "%%memory_tracking") ~= nil
+local IS_CHRONOS_TRACK = IS_DEBUG and string_m.find(sys.get_config_string("log.info_block", ""), "%%chronos_tracking") ~= nil
+
 local time_fn
 local TIME_FORMAT
 if IS_TIME_TRACK then
-	LOGGER_PREFIX = LOGGER_PREFIX .. "%difftimems "
 	--- If microsecond timer is available, use it, otherwise use built-in millisecond timer
 	if chronos then
 		time_fn = chronos.nanotime
-		TIME_FORMAT = "%08.4f"
-		INFO_BLOCK_LENGTH = INFO_BLOCK_LENGTH + 11
+		TIME_FORMAT = "%08.4fms"
 	else
 		time_fn = socket.gettime
-		TIME_FORMAT = "%06.2f"
-		INFO_BLOCK_LENGTH = INFO_BLOCK_LENGTH + 9
+		TIME_FORMAT = "%06.2fms"
 	end
 end
-if IS_MEMORY_TRACK then
-	LOGGER_PREFIX = LOGGER_PREFIX .. "%diffmemkb "
-	INFO_BLOCK_LENGTH = INFO_BLOCK_LENGTH + 7
-end
+
 
 local INFO_BLOCK_DEFAULT = "%levelshort[%logger"
 local MESSAGE_BLOCK_DEFAULT = "]: %message %context <%function>"
 
-local INFO_BLOCK_RAW = sys.get_config_string("log.info_block", INFO_BLOCK_DEFAULT)
+local INFO_BLOCK = sys.get_config_string("log.info_block", INFO_BLOCK_DEFAULT)
 local MESSAGE_BLOCK = sys.get_config_string("log.message_block", MESSAGE_BLOCK_DEFAULT)
-local INFO_BLOCK = LOGGER_PREFIX .. INFO_BLOCK_RAW
 
 local IS_FORMAT_LOGGER = string_m.find(INFO_BLOCK, "%%logger") ~= nil
-local IS_FORMAT_DIFFMEM = string_m.find(INFO_BLOCK, "%%diffmem") ~= nil
-local IS_FORMAT_DIFFTIME = string_m.find(INFO_BLOCK, "%%difftime") ~= nil
 local IS_FORMAT_LEVEL_NAME = string_m.find(INFO_BLOCK, "%%levelname") ~= nil
 local IS_FORMAT_LEVEL_SHORT = string_m.find(INFO_BLOCK, "%%levelshort") ~= nil
 
@@ -71,6 +68,14 @@ local LEVEL_SHORT_NAME = {
 	[ERROR] = "ERROR:", -- Full name used for red highlight in Defold Editor console
 }
 
+local LEVEL_TO_CONSOLE_MAP = {
+	[TRACE] = "TRACE:",
+	[DEBUG] = "DEBUG:",
+	[INFO] = "INFO: ",
+	[WARN] = "WARN: ",
+	[ERROR] = "ERROR:",
+}
+
 local LEVEL_PRIORITY = {
 	[ERROR] = 1,
 	[WARN] = 2,
@@ -82,8 +87,8 @@ local LEVEL_PRIORITY = {
 ---Converts table to one-line string
 ---@param t table
 ---@param depth number
----@param result string|nil @Internal parameter
----@return string, boolean @String representation of table, Is max string length reached
+---@param result string|nil Internal parameter
+---@return string, boolean result String representation of table, Is max string length reached
 local function table_to_string(t, depth, result)
 	if not t then
 		return "", false
@@ -131,43 +136,46 @@ end
 ---@field private _last_message_time number
 local Logger = {}
 
----@param level string @TRACE, DEBUG, INFO, WARN, ERROR
----@param message string @Message to log
----@param context any @Additional data to log
+---@param level string TRACE, DEBUG, INFO, WARN, ERROR
+---@param message string Message to log
+---@param context any Additional data to log
 ---@return string|nil
 function Logger:format(level, message, context)
 	-- Format info block
-	local string_info_block = level == ERROR and INFO_BLOCK_RAW or INFO_BLOCK
+	local string_info_block = INFO_BLOCK
+
 	local current_time = IS_TIME_TRACK and time_fn()
-	if IS_FORMAT_DIFFMEM then
+	if IS_MEMORY_TRACK then
 		local current_memory = collectgarbage("count")
 		local diff_memory = math.max(current_memory - self._last_gc_memory, 0)
 
-		string_info_block = string_m.gsub(string_info_block, "%%diffmem", string.format("%04.1f", diff_memory))
+		string_info_block = string_m.gsub(string_info_block, "%%memory_tracking", string.format("%04.1fkb", diff_memory))
 	end
-	if IS_FORMAT_DIFFTIME then
+	if IS_TIME_TRACK then
 		-- Debug time tracking (in ms)
 		local diff_time = (current_time - self._last_message_time) * 1000
 
-		string_info_block = string_m.gsub(string_info_block, "%%difftime", string.format(TIME_FORMAT, diff_time))
+		string_info_block = string_m.gsub(string_info_block, "%%time_tracking", string.format(TIME_FORMAT, diff_time))
 	end
 	if IS_FORMAT_LOGGER then
-		string_info_block = string_m.gsub(string_info_block, "%%logger", self.name)
+		--string_info_block = string_m.gsub(string_info_block, "%%logger", self.name)
+
+		-- Make logger name length equal to LOGGER_BLOCK_WIDTH
+		local name_to_instert = self.name
+		local logger_name_length = string_m.len(name_to_instert)
+		if logger_name_length < LOGGER_BLOCK_WIDTH then
+			name_to_instert = name_to_instert .. string.rep(" ", LOGGER_BLOCK_WIDTH - logger_name_length)
+		elseif logger_name_length > LOGGER_BLOCK_WIDTH then
+			name_to_instert = string_m.sub(name_to_instert, 1, LOGGER_BLOCK_WIDTH)
+		end
+
+		string_info_block = string_m.gsub(string_info_block, "%%logger", name_to_instert)
 	end
 	if IS_FORMAT_LEVEL_SHORT then
 		string_info_block = string_m.gsub(string_info_block, "%%levelshort", LEVEL_SHORT_NAME[level])
 	end
 	if IS_FORMAT_LEVEL_NAME then
-		string_info_block = string_m.gsub(string_info_block, "%%levelname", level)
-	end
-
-	-- Make string_info_block length equal to INFO_BLOCK_LENGTH
-	local info_block_length = string_m.len(string_info_block)
-	if info_block_length < INFO_BLOCK_LENGTH then
-		string_info_block = string_info_block .. string.rep(" ", INFO_BLOCK_LENGTH - info_block_length)
-	end
-	if info_block_length > INFO_BLOCK_LENGTH then
-		string_info_block = string_m.sub(string_info_block, 1, INFO_BLOCK_LENGTH)
+		string_info_block = string_m.gsub(string_info_block, "%%levelname", LEVEL_TO_CONSOLE_MAP[level])
 	end
 
 	-- Format message block
@@ -195,9 +203,9 @@ function Logger:format(level, message, context)
 end
 
 
----@param level string @TRACE, DEBUG, INFO, WARN, ERROR
----@param message string @Message to log
----@param context any @Additional data to log
+---@param level string TRACE, DEBUG, INFO, WARN, ERROR
+---@param message string Message to log
+---@param context any Additional data to log
 function Logger:log(level, message, context)
 	if LEVEL_PRIORITY[level] > LEVEL_PRIORITY[self.level] then
 		return nil
@@ -214,18 +222,18 @@ function Logger:log(level, message, context)
 		end
 	end
 
-	if IS_FORMAT_DIFFMEM then
+	if IS_MEMORY_TRACK then
 		self._last_gc_memory = collectgarbage("count")
 	end
 
-	if IS_FORMAT_DIFFTIME then
+	if IS_TIME_TRACK then
 		self._last_message_time = time_fn()
 	end
 end
 
 
 ---Log message with TRACE level
----@param message string @Message to log
+---@param message string Message to log
 ---@param data any
 function Logger:trace(message, data)
 	self:log(TRACE, message, data)
@@ -233,7 +241,7 @@ end
 
 
 ---Log message with DEBUG level
----@param message string @Message to log
+---@param message string Message to log
 ---@param data any
 function Logger:debug(message, data)
 	self:log(DEBUG, message, data)
@@ -266,19 +274,19 @@ end
 
 ---Return the new logger instance
 ---@param logger_name string
----@param force_logger_level_in_debug string|nil @Default is DEBUG, values: FATAL, ERROR, WARN, INFO, DEBUG, TRACE
+---@param force_logger_level_in_debug string|nil Default is DEBUG, values: FATAL, ERROR, WARN, INFO, DEBUG, TRACE
 ---@return logger
-function Log.get_logger(logger_name, force_logger_level_in_debug)
+function M.get_logger(logger_name, force_logger_level_in_debug)
 	local instance = {
 		name = logger_name or "",
 		level = force_logger_level_in_debug or GAME_LOG_LEVEL,
 	}
 
-	if IS_FORMAT_DIFFMEM then
+	if IS_MEMORY_TRACK then
 		instance._last_gc_memory = collectgarbage("count")
 	end
 
-	if IS_FORMAT_DIFFTIME then
+	if IS_TIME_TRACK then
 		instance._last_message_time = time_fn()
 	end
 
@@ -292,7 +300,7 @@ function Log.get_logger(logger_name, force_logger_level_in_debug)
 end
 
 
-local DEFAULT_LOGGER = Log.get_logger(sys.get_config_string("project.title", "log"))
-return setmetatable(Log, {
+local DEFAULT_LOGGER = M.get_logger(sys.get_config_string("project.title", "log"))
+return setmetatable(M, {
 	__index = DEFAULT_LOGGER,
 })
