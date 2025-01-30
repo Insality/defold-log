68e1ab6623e8fe364e34bd5f149d3486b813fe12
Insality <insality@gmail.com>
2025-01-30 22:45:34 +0200

    Update output and formatting, change default format

--- log/log.lua
+++ log/log.lua
@@ -4,7 +4,6 @@ local string_m = utf8 or string
 ---@class log
 local M = {}
 
-local TYPE_TABLE = "table"
 local IS_DEBUG = sys.get_engine_info().is_debug
 local SYSTEM_NAME = sys.get_sys_info().system_name
 local IS_MOBILE = SYSTEM_NAME == "iPhone OS" or SYSTEM_NAME == "Android"
@@ -12,9 +11,6 @@ local IS_MOBILE = SYSTEM_NAME == "iPhone OS" or SYSTEM_NAME == "Android"
 local DEFAULT_LEVEL = IS_DEBUG and "DEBUG" or "WARN"
 local GAME_LOG_LEVEL = sys.get_config_string(IS_DEBUG and "log.level" or "log.level_release", DEFAULT_LEVEL)
 
---local IS_TIME_TRACK = IS_DEBUG and sys.get_config_int("log.time_tracking", 0) == 1
---local IS_MEMORY_TRACK = IS_DEBUG and sys.get_config_int("log.memory_tracking", 0) == 1
-local INFO_BLOCK_LENGTH = sys.get_config_int("log.info_block_length", 18)
 local LOGGER_BLOCK_WIDTH = sys.get_config_int("log.logger_block_width", 10)
 local MAX_LOG_LENGTH = sys.get_config_int("log.max_log_length", 1024)
 local INSPECT_DEPTH = sys.get_config_int("log.inspect_depth", 1)
@@ -25,29 +21,19 @@ local IS_TIME_TRACK = IS_DEBUG and string_m.find(sys.get_config_string("log.info
 local IS_MEMORY_TRACK = IS_DEBUG and string_m.find(sys.get_config_string("log.info_block", ""), "%%memory_tracking") ~= nil
 local IS_CHRONOS_TRACK = IS_DEBUG and string_m.find(sys.get_config_string("log.info_block", ""), "%%chronos_tracking") ~= nil
 
-local time_fn
-local TIME_FORMAT
-if IS_TIME_TRACK then
-	--- If microsecond timer is available, use it, otherwise use built-in millisecond timer
-	if chronos then
-		time_fn = chronos.nanotime
-		TIME_FORMAT = "%08.4fms"
-	else
-		time_fn = socket.gettime
-		TIME_FORMAT = "%06.2fms"
-	end
-end
+-- Info: %levelname[%logger]
+-- Message: %tab%message: %context %tab<%function>
+-- Preview: DEBUG:[game.logger     ]	Debug message: {debug: message, value: 2} 	<example/example.gui_script:17>
 
+-- Info: %levelname| %time_tracking | %memory_tracking | %logger
+-- Message: | %tab%message: %context %tab<%function>
+-- Preview: DEBUG:| 166.71ms |   2.4kb | game.logger      :	Delayed message: just string 	<example/example.gui_script:39>
 
-local INFO_BLOCK_DEFAULT = "%levelshort[%logger"
-local MESSAGE_BLOCK_DEFAULT = "]: %message %context <%function>"
-
-local INFO_BLOCK = sys.get_config_string("log.info_block", INFO_BLOCK_DEFAULT)
-local MESSAGE_BLOCK = sys.get_config_string("log.message_block", MESSAGE_BLOCK_DEFAULT)
+local INFO_BLOCK = sys.get_config_string("log.info_block", "%levelname| %time_tracking | %memory_tracking | %logger")
+local MESSAGE_BLOCK = sys.get_config_string("log.message_block", "%tab%message %context %tab<%function>")
 
 local IS_FORMAT_LOGGER = string_m.find(INFO_BLOCK, "%%logger") ~= nil
 local IS_FORMAT_LEVEL_NAME = string_m.find(INFO_BLOCK, "%%levelname") ~= nil
-local IS_FORMAT_LEVEL_SHORT = string_m.find(INFO_BLOCK, "%%levelshort") ~= nil
 
 local IS_FORMAT_TAB = string_m.find(MESSAGE_BLOCK, "%%tab") ~= nil
 local IS_FORMAT_MESSAGE = string_m.find(MESSAGE_BLOCK, "%%message") ~= nil
@@ -59,14 +45,7 @@ local DEBUG = "DEBUG"
 local INFO = "INFO"
 local WARN = "WARN"
 local ERROR = "ERROR"
-
-local LEVEL_SHORT_NAME = {
-	[TRACE] = "T",
-	[DEBUG] = "D",
-	[INFO] = "I",
-	[WARN] = "W",
-	[ERROR] = "ERROR:", -- Full name used for red highlight in Defold Editor console
-}
+local FATAL = "FATAL"
 
 local LEVEL_TO_CONSOLE_MAP = {
 	[TRACE] = "TRACE:",
@@ -74,9 +53,11 @@ local LEVEL_TO_CONSOLE_MAP = {
 	[INFO] = "INFO: ",
 	[WARN] = "WARN: ",
 	[ERROR] = "ERROR:",
+	[FATAL] = "FATAL:",
 }
 
 local LEVEL_PRIORITY = {
+	[FATAL] = 0, -- Used to disable logs
 	[ERROR] = 1,
 	[WARN] = 2,
 	[INFO] = 3,
@@ -106,7 +87,7 @@ local function table_to_string(t, depth, result)
 			result = result .. ", "
 		end
 
-		if type(value) == TYPE_TABLE then
+		if type(value) == "table" then
 			if depth == 0 then
 				result = result .. key .. ": {... #" .. #value .. "}"
 			else
@@ -144,25 +125,49 @@ function Logger:format(level, message, context)
 	-- Format info block
 	local string_info_block = INFO_BLOCK
 
-	local current_time = IS_TIME_TRACK and time_fn()
 	if IS_MEMORY_TRACK then
+		local format = "%5.1fkb"
 		local current_memory = collectgarbage("count")
-		local diff_memory = math.max(current_memory - self._last_gc_memory, 0)
+		local diff_memory = current_memory - self._last_gc_memory
 
-		string_info_block = string_m.gsub(string_info_block, "%%memory_tracking", string.format("%04.1fkb", diff_memory))
+		if diff_memory < 0 then
+			-- It's because of garbage collector
+			format = "    ..."
+		end
+
+		if diff_memory > 1000 then
+			diff_memory = diff_memory / 1000
+			format = "%4.1f mb"
+		end
+
+		string_info_block = string_m.gsub(string_info_block, "%%memory_tracking", string.format(format, diff_memory))
 	end
 	if IS_TIME_TRACK then
-		-- Debug time tracking (in ms)
-		local diff_time = (current_time - self._last_message_time) * 1000
+		local format = "%6.2fms"
+		local diff_time = (socket.gettime() - self._last_message_time) * 1000
+		if diff_time > 1000 then
+			diff_time = diff_time / 1000
+			format = "%6.2f s"
+		end
 
-		string_info_block = string_m.gsub(string_info_block, "%%time_tracking", string.format(TIME_FORMAT, diff_time))
+		string_info_block = string_m.gsub(string_info_block, "%%time_tracking", string.format(format, diff_time))
 	end
-	if IS_FORMAT_LOGGER then
-		--string_info_block = string_m.gsub(string_info_block, "%%logger", self.name)
 
+	if IS_CHRONOS_TRACK then
+		local format = "%8.4fms"
+		local diff_time = (chronos.nanotime() - self._last_message_time) * 1000
+		if diff_time > 1000 then
+			diff_time = diff_time / 1000
+			format = "%8.4f s"
+		end
+
+		string_info_block = string_m.gsub(string_info_block, "%%chronos_tracking", string.format(format, diff_time))
+	end
+
+	if IS_FORMAT_LOGGER then
 		-- Make logger name length equal to LOGGER_BLOCK_WIDTH
 		local name_to_instert = self.name
-		local logger_name_length = string_m.len(name_to_instert)
+		local logger_name_length = string_m.len(self.name)
 		if logger_name_length < LOGGER_BLOCK_WIDTH then
 			name_to_instert = name_to_instert .. string.rep(" ", LOGGER_BLOCK_WIDTH - logger_name_length)
 		elseif logger_name_length > LOGGER_BLOCK_WIDTH then
@@ -171,9 +176,7 @@ function Logger:format(level, message, context)
 
 		string_info_block = string_m.gsub(string_info_block, "%%logger", name_to_instert)
 	end
-	if IS_FORMAT_LEVEL_SHORT then
-		string_info_block = string_m.gsub(string_info_block, "%%levelshort", LEVEL_SHORT_NAME[level])
-	end
+
 	if IS_FORMAT_LEVEL_NAME then
 		string_info_block = string_m.gsub(string_info_block, "%%levelname", LEVEL_TO_CONSOLE_MAP[level])
 	end
@@ -183,17 +186,20 @@ function Logger:format(level, message, context)
 	if IS_FORMAT_TAB then
 		string_message_block = string_m.gsub(string_message_block, "%%tab", "\t")
 	end
+
 	if IS_FORMAT_MESSAGE then
 		string_message_block = string_m.gsub(string_message_block, "%%message", message)
 	end
+
 	if IS_FORMAT_CONTEXT then
 		local record_context = ""
 		if context ~= nil then
-			local is_table = type(context) == TYPE_TABLE
+			local is_table = type(context) == "table"
 			record_context = is_table and table_to_string(context, INSPECT_DEPTH) or tostring(context)
 		end
 		string_message_block = string_m.gsub(string_message_block, "%%context", record_context)
 	end
+
 	if IS_FORMAT_FUNCTION then
 		local caller_info = debug.getinfo(4)
 		string_message_block = string_m.gsub(string_message_block, "%%function", caller_info.short_src .. ":" .. caller_info.currentline)
@@ -227,7 +233,11 @@ function Logger:log(level, message, context)
 	end
 
 	if IS_TIME_TRACK then
-		self._last_message_time = time_fn()
+		self._last_message_time = socket.gettime()
+	end
+
+	if IS_CHRONOS_TRACK then
+		self._last_message_time = chronos.nanotime()
 	end
 end
 
@@ -287,7 +297,11 @@ function M.get_logger(logger_name, force_logger_level_in_debug)
 	end
 
 	if IS_TIME_TRACK then
-		instance._last_message_time = time_fn()
+		instance._last_message_time = socket.gettime()
+	end
+
+	if IS_CHRONOS_TRACK then
+		instance._last_message_time = chronos.nanotime()
 	end
 
 	if not IS_DEBUG then
